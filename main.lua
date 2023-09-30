af896b6b6d2ebd56b64cc869bf8b228889bc875a
Zorin <ZorinGit@proton.me>
2023-09-29 21:18:49 -0400

    basic prototype

--- main.lua
+++ main.lua
@@ -1,2 +1,20 @@
-print("Hello World!!!!")
-print("last test")
+function love.load()
+    LANDER = {}
+    LANDER.x = 400
+    LANDER.y = 40
+end
+
+function love.update(dt)
+    -- lander if falling
+    LANDER.y = LANDER.y + 50 * dt
+
+    -- up trust
+    if love.keyboard.isDown("s") then
+        LANDER.y = LANDER.y - 60 * dt
+    end
+end
+
+function love.draw()
+    -- draw lander
+    love.graphics.circle("fill", LANDER.x, LANDER.y, 20)
+end
