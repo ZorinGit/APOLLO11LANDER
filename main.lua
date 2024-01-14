4cb265b32dff392da6e700a841bb66c2f2d0a4cf
Zorin <ZorinGit@proton.me>
2024-01-14 01:28:56 -0500

    fixed fuel lvl1

--- main.lua
+++ main.lua
@@ -131,7 +131,7 @@ function love.load()
         lander_x_velocity = 0,
         lander_y = 50,
         lander_y_velocity = 0,
-        lander_fuel_s = 33,
+        lander_fuel_s = 99,
         surface_line_points = {0, 750, 1280, 750},
         landing_surface_line_points = {300, 748, 350, 748}
     }
